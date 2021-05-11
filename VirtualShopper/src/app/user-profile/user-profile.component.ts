114042bc8ef081bacf7c33272a7140ce87d15dd3
Michch333 <Michch333@gmail.com>
2021-05-10 23:43:58 -0400

    TS value and mapping

--- VirtualShopper/src/app/user-profile/user-profile.component.ts
+++ VirtualShopper/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserProfile } from '../interfaces'
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+   }
+
+  userProfile: UserProfile = {
+    profilePictureURL: '',
+    userName: '',
+    firstName: '',
+    lastName: '',
+    address: null,
+    phoneNumber: 0,
+    bio: '',
+    shirtSize: '',
+    shirtFit: '',
+    pantLength: 0,
+    pantWidth: 0,
+    pantSize: '',
+    shoeSize: 0
+  };
+
+
+createProfile(firstName: string, lastName: string, bio: string, shirtSize: string, shirtFit: string, phoneNumber: number){
+  this.userProfile.firstName = firstName;
+  this.userProfile.lastName = lastName;
+  this.userProfile.bio = bio;
+  this.userProfile.shirtSize = shirtSize;
+  this.userProfile.shirtFit = shirtFit;
+  this.userProfile.phoneNumber = phoneNumber;
+
+  this.printProfile();
+}
+
+printProfile(){
+  console.log(this.userProfile);
+}
 
   ngOnInit(): void {
   }
 
+
 }
+;
