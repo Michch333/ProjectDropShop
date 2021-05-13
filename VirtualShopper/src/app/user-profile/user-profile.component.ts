57385c1bfbb41c74db7ddfda3cda589468e7de65
Jacob Plumb <jacobmaxplumb@gmail.com>
2021-05-12 20:21:09 -0400

    updated form to use formgroup and refactored other spots

--- VirtualShopper/src/app/user-profile/user-profile.component.ts
+++ VirtualShopper/src/app/user-profile/user-profile.component.ts
@@ -1,51 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { UserProfile } from '../interfaces'
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserProfile } from 'src/models/user-profile';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
+  public shirtSizes: string[] = ['XS', 'S', 'M', 'L', 'XL'];
+  public shirtFits: string[] = ['Tight', 'Loose'];
+  public userProfileForm: FormGroup = new FormGroup({
+    firstName: new FormControl(null, [Validators.required]),
+    lastName: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.email, Validators.required]),
+    bio: new FormControl(null, [Validators.required]),
+    shirtSize: new FormControl(null, [Validators.required]),
+    shirtFit: new FormControl(null, [Validators.required]),
+    phoneNumber: new FormControl(null, [Validators.required]),
+  });;
 
-  constructor() {
-   }
+  constructor() {}
 
-  userProfile: UserProfile = {
-    profilePictureURL: '',
-    userName: '',
-    firstName: '',
-    lastName: '',
-    address: null,
-    phoneNumber: 0,
-    bio: '',
-    shirtSize: '',
-    shirtFit: '',
-    pantLength: 0,
-    pantWidth: 0,
-    pantSize: '',
-    shoeSize: 0
-  };
-
-
-createProfile(firstName: string, lastName: string, bio: string, shirtSize: string, shirtFit: string, phoneNumber: number){
-  this.userProfile.firstName = firstName;
-  this.userProfile.lastName = lastName;
-  this.userProfile.bio = bio;
-  this.userProfile.shirtSize = shirtSize;
-  this.userProfile.shirtFit = shirtFit;
-  this.userProfile.phoneNumber = phoneNumber;
-
-  this.printProfile();
-}
-
-printProfile(){
-  console.log(this.userProfile);
-}
-
-  ngOnInit(): void {
+  public cancelled(e) {
+    e.preventDefault();
+    this.userProfileForm.reset();
   }
 
+  public profileFormSubmitted() {
+    console.log(this.userProfileForm);
+  }
 
+  public ngOnInit(): void {
+  }
 }
-;
